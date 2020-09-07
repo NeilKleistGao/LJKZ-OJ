f71c1f0e56f72752b028af332a8827a7319e223b
ER_Krain <1965287334@qq.com>
2020-09-07 15:47:21 +0800

    add competion/competitionSet/createCompetition/submit web file

---
+++ LJKZOJ/src/main/java/bean/CompetitionBean.java
@@ -0,0 +1,62 @@
+package bean;
+
+import utils.Entry;
+
+import javax.annotation.ManagedBean;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import java.util.Base64;
+import java.util.Date;
+import java.util.Map;
+
+@ManagedBean
+@RequestScoped
+public class CompetitionBean {
+
+    private int NUMBER_OF_PROBLEMS;
+    private Entry[] entries = new Entry[NUMBER_OF_PROBLEMS];
+    private int userAC;
+    private int userPenalty;
+    private int rank;
+
+    public int getNUMBER_OF_PROBLEMS() {
+        return NUMBER_OF_PROBLEMS;
+    }
+
+    public void setNUMBER_OF_PROBLEMS(int NUMBER_OF_PROBLEMS) {
+        this.NUMBER_OF_PROBLEMS = NUMBER_OF_PROBLEMS;
+    }
+
+    public Entry[] getEntries() {
+        return entries;
+    }
+
+    public void setEntries(Entry[] entries) {
+        this.entries = entries;
+    }
+
+    public int getUserAC() {
+        return userAC;
+    }
+
+    public void setUserAC(int userAC) {
+        this.userAC = userAC;
+    }
+
+    public int getUserPenalty() {
+        return userPenalty;
+    }
+
+    public void setUserPenalty(int userPenalty) {
+        this.userPenalty = userPenalty;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+}
---
+++ LJKZOJ/src/main/java/bean/CompetitionSetBean.java
@@ -0,0 +1,139 @@
+package bean;
+
+
+import utils.Pagination;
+import utils.CompEntry;
+
+import javax.annotation.ManagedBean;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+@ManagedBean
+@RequestScoped
+public class CompetitionSetBean {
+    private final static int NUMBER_OF_ENTRIES_IN_PAGE = 16;
+
+    private int pageNumber;
+    private int totalNumber;
+    private CompEntry[] compEntries = new CompEntry[NUMBER_OF_ENTRIES_IN_PAGE];
+    private String searchfor="Finished";
+    private Pagination[] paginations = null;
+
+    public CompEntry[] getcompEntries() {
+        return compEntries;
+    }
+
+    public void setcompEntries(CompEntry[] compEntries) {
+        this.compEntries = compEntries;
+    }
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    public int getTotalNumber() {
+        return totalNumber;
+    }
+
+    public void setTotalNumber(int totalNumber) {
+        this.totalNumber = totalNumber;
+    }
+
+    public String getSearchfor() {
+        return searchfor;
+    }
+
+    public void setSearchfor(String searchfor) {
+        this.searchfor = searchfor;
+    }
+
+    public Pagination[] getPaginations() {
+        return paginations;
+    }
+
+    public void setPaginations(Pagination[] paginations) {
+        this.paginations = paginations;
+    }
+
+    private void setuppagination(){
+        this.paginations[0].setNotation("<<");
+        this.paginations[this.paginations.length - 1].setNotation(">>");
+        if (this.pageNumber == 1) {
+            this.paginations[0].setLink("#");
+            this.paginations[0].setStyle("page-item disabled");
+        }
+        else {
+            this.paginations[0].setLink("/problems.xhtml?page=" + String.valueOf(this.pageNumber - 1));
+            this.paginations[0].setStyle("page-item");
+        }
+
+        if (this.pageNumber == this.totalNumber) {
+            this.paginations[this.paginations.length - 1].setLink("#");
+            this.paginations[this.paginations.length - 1].setStyle("page-item disabled");
+        }
+        else {
+            this.paginations[this.paginations.length - 1].setLink("/problems.xhtml?page=" + String.valueOf(this.pageNumber + 1));
+            this.paginations[this.paginations.length - 1].setStyle("page-item");
+        }
+
+        int start, end;
+        if (this.totalNumber == 0) {
+            start = 1; end = 1;
+        }
+        else if (this.totalNumber < 5) {
+            start = 1; end = this.totalNumber;
+        }
+        else if (this.pageNumber < 3) {
+            start = 1; end = 5;
+        }
+        else if (this.pageNumber + 2 > this.totalNumber) {
+            start = this.totalNumber - 4; end = this.totalNumber;
+        }
+        else {
+            start = this.pageNumber - 2; end = this.pageNumber + 2;
+        }
+
+        for (int i = 1, j = start; j <= end; i++, j++) {
+            this.paginations[i].setNotation(String.valueOf(j));
+            if (j == this.pageNumber) {
+                this.paginations[i].setLink("#");
+                this.paginations[i].setStyle("page-item activate");
+            }
+            else {
+                this.paginations[i].setLink("/problems.xhtml?page=" + String.valueOf(j));
+                this.paginations[i].setStyle("page-item");
+            }
+        }
+    }
+
+    public void init(){
+
+    }
+
+    public void CompStateChange(ValueChangeEvent e){
+        searchfor = e.getNewValue().toString();
+
+        FacesContext context = FacesContext.getCurrentInstance();
+        ExternalContext ex = context.getExternalContext();
+
+        try {
+            ex.redirect(
+                    "/LJKZOJ-1.0-SNAPSHOT/problems.xhtml?page=1&for="
+                            + searchfor);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+}
